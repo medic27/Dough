073d9552b0bda524a3513571ef871f98095735ec
yantonsoup <ygoldobin@gmail.com>
2016-09-14 12:31:14 -0700

    Merged with jayce/simon, now bringing up our changes

--- client/nav.js
+++ client/nav.js
@@ -10,9 +10,6 @@ export default class Nav extends React.Component {
           <li><Link to="/dashboard">Dashboard</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/signup">Signup</Link></li>
-
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/expense">Expenses</Link></li>
           <li><Link to="/goals">Goals</Link></li>
 
         </ul>
